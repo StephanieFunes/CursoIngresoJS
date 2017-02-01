a29572ba43bbc941f67e86a79ade2089672b091b
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-01 12:01:28 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+// Esto es un comentario de una línea
+
+var nombre;
+nombre = "Stephi";//Defino variable
+nombre = prompt ("Ingrese su nombre","Natalia Natalia");
+alert ("nombre: "+ nombre);
+
+	var person = prompt("Ingresar tu nombre", "Natalia Natalia");
 }
 
