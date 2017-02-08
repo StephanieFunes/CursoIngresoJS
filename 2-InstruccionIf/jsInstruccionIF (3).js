5fc200ecad0d07b5c96ea0516b8855b72b108390
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-08 11:50:59 -0300

    Ejercicio 3

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,23 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+
+	edad = document.getElementById('edad').value;
+	
+	if(edad<18)
+		{
+			alert ("Usted es menor de edad");
+
+		}
+
+	else 
+		{
+			alert ("Usted es mayor de edad");
+
+		}
+
+
+
 
 }//FIN DE LA FUNCIÓN
