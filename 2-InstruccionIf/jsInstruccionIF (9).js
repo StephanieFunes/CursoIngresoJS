352f4e4f36cb8eb99e4285148ffc20906b815dc8
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-14 10:30:42 -0300

    Ejercicio 9

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,15 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var random;
+
+	random = Math.floor(Math.random() * 10) + 1;
+
+	alert(random);
 
 }//FIN DE LA FUNCIÓN
+
+/*
+Math.floor // devuelve un número entero
+Math.floor(Math.random() * 100); // returns a number between 0 and 99
+Math.floor(Math.random() * 10) + 1;  // returns a number between 1 and 10 */
