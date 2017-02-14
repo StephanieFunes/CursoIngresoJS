f79257ab210e8911488a5ac2a4015367ae6e0e9e
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-14 12:06:34 -0300

    Ejercicio 2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,33 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	switch(mesDelAño)
+
+		{
+			case"Enero":
+			case"Febrero":
+			case"Marzo":
+			case"Abril":
+			case"Mayo":
+			case"Junio":
+			alert("Todavía no llegó el invierno");
+			break;
+
+			case"Julio":
+			case"Agosto":
+			alert("Abrigate que hace frío");
+			break;
+
+			case"Septiembre":
+			case"Octubre":
+			case"Noviembre":
+			case"Diciembre":
+			alert("Ya pasamos el frío, ahora calor!!");
+			break;
+		}	
+
+
+	
 
 
 
