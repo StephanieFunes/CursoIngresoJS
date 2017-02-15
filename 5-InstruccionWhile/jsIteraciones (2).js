06885a88ec48bb73b57cb1fa048cb2cfef96db48
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-15 12:17:03 -0300

    Ejercicio 2

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -2,5 +2,16 @@ function Mostrar()
 {
 	alert('iteración while');
 
+	var contador = 10;
+
+	while(contador > 0)
+	
+	{
+		document.write ("número: " + contador + "<br>");
+		contador--;
+		
+		console.log(contador);
+	}
+
 
 }//FIN DE LA FUNCIÓN
