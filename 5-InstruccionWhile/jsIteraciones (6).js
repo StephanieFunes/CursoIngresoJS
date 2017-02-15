2e37e31ae6d979da265883ad8ad0789897015ff6
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-15 12:18:36 -0300

    Ejercicio 6

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,7 +3,38 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	var promedio;
+	var negativo;
 
+	while(contador<5)
+	{
+		contador++;
+		numero = prompt("Ingrese un número");
+		numero = parseInt(numero);
+
+		while(numero < -10 ||numero >10)
+		{
+			numero = prompt("Error.Reingrese el número");
+			numero = parseInt(numero);
+		}//Fin while(numero < -10 ||numero >10)
+
+			if(numero<0)
+			{
+				negativo = negativo * numero;
+
+			}
+			else{
+
+				acumulador= acumulador+numero;
+
+			}
+	acumulador=acumulador+numero
+
+
+	}
+
+	promedio = acumulador/5;//ó acumulador/contador; Debe ir afuera de la función porque sino haría la opreación 5 veces
 
 
 document.getElementById('suma').value=acumulador;
