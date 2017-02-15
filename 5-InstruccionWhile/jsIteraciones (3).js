679fa3d0d07ce86d4f0932986cb5f3385c59e866
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-15 12:17:22 -0300

    Ejercicio 3

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,15 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+	while(!(clave == "utn750"))
+	{
+		clave = prompt("Clave inválida.Reingrese su clave por favor");
+		
+	}
+
+	alert("Bienvenido");
+
+
+
 
 }//FIN DE LA FUNCIÓN
