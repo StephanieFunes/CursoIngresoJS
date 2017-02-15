159a34b08fdf13d12e9e9f600a1af8c2d8d6b03f
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-15 12:18:10 -0300

    Ejercicio 5

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,7 +3,11 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+while(!(sexo == "f" || sexo == "m"))
+	{
+		sexo = prompt("Inválido. Ingrese f ó m ");
 
+	}
 
 document.getElementById('Sexo').value=sexo;
 
