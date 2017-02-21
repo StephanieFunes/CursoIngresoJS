6513c7ebe33c0052b2c36b649461af7ebe9f947a
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-21 20:28:47 -0300

    Ejercicio 6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,5 +1,31 @@
 function Mostrar()
 {
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if(edad>18)
+	{
+		alert("Usted es mayor de edad");
+	}
+
+	else
+	{
+		if(edad<13)
+		{
+			alert("Usted es un niño");
+		}
+
+		else
+		{
+			alert("Usted es adolescente");
+		}
+
+	}
+}
+
+/*function Mostrar()
+{
 //tomo la edad  
 	var edad;
 
@@ -26,4 +52,4 @@ function Mostrar()
 
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN*/
