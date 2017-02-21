d37778ff07e59181da528b8bf256571f0b966b67
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-21 12:05:13 -0300

    Ejercicio 9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,45 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador= 0;
+	var numero;
+	var max;
+	var min;
 	// declarar variables
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
-	{
-		
+	{		
+		numero = prompt("Ingrese un número");
+		numero = parseInt(numero);
+		respuesta = prompt("¿Desea continuar?");
+				
+		if(contador == 0)
+		{
+			max = numero;
+			min = numero;
+		}
+
+		else
+		{
+ 			if(numero > max)
+ 			{
+ 				max = numero;
+
+ 			}
+
+ 			if(numero < min)
+ 			{
+ 				min = numero;
+
+ 			}
+
+		}
 	
 	}
 
-
-
+	document.getElementById('maximo').value = max;
+	document.getElementById('minimo').value = min;
 
 }//FIN DE LA FUNCIÓN
