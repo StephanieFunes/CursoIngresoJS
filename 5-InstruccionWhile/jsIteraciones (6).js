bdfea302c5045a1e10b76a430b3ab3d554802126
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-21 09:37:48 -0300

    Update jsIteraciones (6).js

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -10,12 +10,12 @@ function Mostrar()
 	while(contador<5)
 	{
 		contador++;
-		numero = prompt("Ingrese un número");
+		numero = prompt("Ingrese un número mayor a -10 y menor a 10");
 		numero = parseInt(numero);
 
 		while(numero < -10 ||numero >10)
 		{
-			numero = prompt("Error.Reingrese el número");
+			numero = prompt("Error.Reingrese un número mayor a -10 y menor a 10");
 			numero = parseInt(numero);
 		}//Fin while(numero < -10 ||numero >10)
 
@@ -29,9 +29,7 @@ function Mostrar()
 				acumulador= acumulador+numero;
 
 			}
-	acumulador=acumulador+numero
-
-
+	
 	}
 
 	promedio = acumulador/5;//ó acumulador/contador; Debe ir afuera de la función porque sino haría la opreación 5 veces
