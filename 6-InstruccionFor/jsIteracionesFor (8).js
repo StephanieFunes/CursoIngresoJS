152c7c97d80e8b728285cc1cc3b84b7d9efa34e4
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-22 12:03:01 -0300

    Nuevo ejercicio 8

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,12 @@
 function Mostrar()
 {
+	/* Al generar números random del 0 al 10 se debe contar la cantida de cada una de esta cifras, repetir la iteración 
+	para lanzar el random 100.000 veces e informar:
+
+	1- La cantidad de veces que salió el número del 0 al 10
+	2- El porcentaje de veces que salió cada número con respecto al total.*/
+
+	 
 
 
 
