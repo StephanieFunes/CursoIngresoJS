d35a9f9e07e5e4aec5978632c9aee717b7cc5c5d
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-22 11:54:51 -0300

    Ejercicio 3

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,12 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
 
-
+for(var contador=0; contador < repeticiones; contador++ )
+{
+	alert("Hola UTN FRA");
+}
+	
 
 }//FIN DE LA FUNCIÓN
