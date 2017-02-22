79e0149e57f534236a2e97ab7cc1730cbe548c00
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-22 00:05:23 -0300

    Ejercicio 7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -3,15 +3,12 @@ function Mostrar()
 //tomo la edad  
 	var edad;
 	var estadoCivil;
-	var soltero;
-	var casado;
-	var divorciado;
 
 	edad = document.getElementById('edad').value;
 	estadoCivil = document.getElementById('estadoCivil').value;
 
-	if(edad < 18 && estadoCivil != soltero){
-
+	if(edad < 18 && estadoCivil != "Soltero")
+	{
 		alert("Usted es muy pequeño para no ser soltero");
 	}
 
