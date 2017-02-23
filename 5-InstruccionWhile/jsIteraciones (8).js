f618e3df01bb5c72205b2488b18b02b9ecc71937
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-23 01:34:02 -0300

    Ejercicio 8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,33 @@
 function Mostrar()
 {
-
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
 
+	while(respuesta == "si")
+	{
+		contador++;
+		numero = prompt("Ingrese un número");
+		numero = parseInt(numero);
+		respuesta = prompt("Desea ingresar otro número?");
+		
+		if(numero > 0)
+		{
+			positivo = positivo + numero;
+		}
+		else
+		{
+			negativo = negativo * numero;
+		}
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		console.log("El negativo es " + negativo);
+		console.log("El positivo es " + positivo);
+	}
+
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
