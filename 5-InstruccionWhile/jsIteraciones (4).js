dbe3c351a81c574c5573875d2acf583080f1aa37
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-22 23:07:05 -0300

    Ejercicio 4

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,14 +1,26 @@
 function Mostrar()
 {
+	var numero;
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	numero = prompt("Ingrese un número del 0 al 9");
+
+	while(!(numero > -1 && numero < 10))
+	{
+		numero = prompt ("Error.Intentelo nuevamente");
+
+	}
+
+	document.getElementById('Numero').value = numero;
+
+
+	/*var numero = prompt("ingrese un número entre 0 y 10.");
 
 	while(!(numero>-1 && numero<10))
 		{
 			numero = prompt("Número inválido. Ingrese un número de 0 al 9");
 		}
 
-	alert("El número ingresado es " + numero);
+	alert("El número ingresado es " + numero);*/
 
 
 }//FIN DE LA FUNCIÓN
