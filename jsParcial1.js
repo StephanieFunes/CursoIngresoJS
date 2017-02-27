01ae155d96d59cc3b2da5d34ba76bc6a8cefcf47
StephanieFunes <stefifunes_90@hotmail.com>
2017-02-27 19:54:43 -0300

    Parcial 1

---
+++ jsParcial1.js
@@ -0,0 +1,210 @@
+function Nivel1Ej1() {
+
+	var base;
+	var perimetro;
+
+	base = document.getElementById('baseDelCuadrado').value ;
+	base = parseInt(base);
+	perimetro = 4 * base;
+
+	alert("El perímetro es " + perimetro);
+
+}
+
+function Nivel1Ej2() {
+
+	var importe;
+	var importeFinal;
+
+	importe = parseInt(importe);
+	importe = prompt ("Introduzca el importe de un producto");
+
+	importeFinal = importe * 1.21;
+
+	alert("El importe final es " + importeFinal);
+}
+
+function Nivel1Ej3() {
+
+	var largo;
+	var ancho;
+	var perimetro;
+
+	largo = parseInt(largo);
+	ancho = parseInt (ancho);
+
+	largo = document.getElementById('largoDelTerreno').value;
+	ancho = document.getElementById('anchoDelTerreno').value;
+
+	perimetro = (2 * ancho + 2 * largo) * 3;
+
+	alert("Se necesitan " + perimetro + "metros");
+		
+}
+
+function Nivel2Ej4() {
+
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	
+	numeroUno = prompt("Ingrese un número");
+	numeroDos = prompt ("Ingrese un número");
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	if(numeroUno == numeroDos)
+	{
+		resultado = numeroUno * numeroDos;
+	}
+
+	if(numeroUno > numeroDos)
+	{
+		resultado = numeroUno - numeroDos;
+	}
+
+	if(numeroUno < numeroDos)
+	{
+		resultado = numeroUno + numeroDos;
+	}
+
+	document.write("El resultado es: " + resultado);
+	
+}
+
+function Nivel2Ej5() {
+	
+	var dia;
+
+	dia = prompt("Ingrese un día de la semana")
+	{
+		switch(dia)
+		{
+			case "lunes":
+			case "martes":
+			case "miércoles":
+			case "jueves":
+			case "viernes":
+
+			alert("a trabajar");
+			break;
+			
+			case "sábado":
+			case "domingo":
+
+			alert("Es fin de semana");
+			break;
+
+		}
+
+	}
+
+}
+
+function Nivel3Ej6() {
+	
+	var contador = 0;
+	var importe;
+	var mayorImporte;
+	var menorImporte;
+
+	while(contador < 24)
+	{
+		importe = prompt("Ingrese un importe");
+
+		if(importe > 0)
+		{
+			if (contador == 0)
+			{	
+				mayorImporte = importe;
+				menorImporte = importe;
+				
+			}
+
+			else
+			{
+				if(importe > mayorImporte)
+				{
+					mayorImporte = importe;
+
+				}
+				if(importe < menorImporte)
+				{
+					menorImporte = importe;
+
+				}
+
+			}
+
+			contador ++;
+		}
+
+		else
+		{
+			alert("Número inválido");
+		}		
+	} 
+
+	document.write("El mayor importe es " + mayorImporte + "<br>");
+	document.write("El menor importe es " + menorImporte + "<br>");
+
+}
+
+function Nivel3Ej7() {
+
+	var nota;
+	var sexo;
+	var contador = 0;
+	var sumaNotas = 0;
+	var promedioNotas;
+	var menorNota;
+	var hombresAprobados = 0;
+
+	while(contador < 5)
+	{
+		nota = prompt("Ingrese su nota " + (contador+1));
+		nota = parseInt(nota);		
+		
+		while(!(nota >= 0 && nota <= 10))
+		{
+			nota = prompt("Error. Ingrese su nota");
+			nota = parseInt(nota);
+			
+		}
+
+		sexo = prompt("Ingrese su sexo " + (contador+1));
+
+		while (sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("Error. Ingrese su sexo"); 
+
+		}
+
+		if(contador == 0 || nota < menorNota)
+		{
+			menorNota = nota;
+		}
+
+		if(sexo=="m" && nota >= 6)
+		{
+			hombresAprobados = hombresAprobados + 1;
+		}
+
+		sumaNotas = sumaNotas + nota;
+
+		contador ++;
+
+
+	}
+	
+	promedioNotas = sumaNotas / contador;
+
+	alert ("El promedio de notas es " + promedioNotas + "\nLa menor nota es " + menorNota + "\nLos hombres con nota mayor o igual a 6 son " + hombresAprobados);
+
+
+}
+
+function Nivel4Ej8() {
+	
+}
