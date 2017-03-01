f9a919054c6e5eb653a006864d9c45a0641677f1
StephanieFunes <stefifunes_90@hotmail.com>
2017-03-01 10:05:04 -0300

    Ejercicio 8

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -5,6 +5,45 @@ function Mostrar()
 
 	1- La cantidad de veces que salió el número del 0 al 10
 	2- El porcentaje de veces que salió cada número con respecto al total.*/
+	var numAnterior;
+	var numIngresado;
+	var numRecorrido;
+	var tieneDivisor = 'no';
+
+	numIngresado = prompt("Ingrese un número");
+	numIngresado = parseInt(numIngresado);
+
+	for(numRecorrido = numIngresado -1; numRecorrido >2; numRecorrido--)
+	{
+		tieneDivisor = "no";
+
+		for(numAnterior = 2; numAnterior<(numIngresado/2); numAnterior++)
+			{
+				if (numIngresado % numAnterior == 0)
+				{
+					tieneDivisor = "si";
+					break;
+
+				}
+			}
+
+			if(tieneDivisor == "no")
+			{
+				console.log("Es primo " + numIngresado);
+			}
+
+			else
+			{
+				console.log("No es primo " + numIngresado);
+			}
+	}
+
+
+			
+
+
+
+
 
 	 
 
